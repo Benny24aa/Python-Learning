233faa7afd0f843ffb6adb3edcbcbdfafc199571
Benny24aa <harleyb101010@gmail.com>
2025-02-23 09:00:39 +0000

    added columns being selected, removing unncessary columns

--- Python Learning.py
+++ Python Learning.py
@@ -1,3 +1,6 @@
 import pandas as pd
-Diagnostics = pd.read_csv('diagnostics_by_board_september_2024.csv')
-Diagnostics = Diagnostics[Diagnostics['HBT'] == "S08000015"]
+Diagnostics = pd.read_csv('diagnostics_by_board_september_2024.csv') #### Reading Data
+Diagnostics = Diagnostics[Diagnostics['HBT'] == "S08000015"] ### Filtering
+
+Diagnostics = Diagnostics[['MonthEnding', 'DiagnosticTestType', 'DiagnosticTestDescription', 'WaitingTime', 'NumberOnList']]
+### Selecting columns required, just like select in R
