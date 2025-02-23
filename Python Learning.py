17aa6327b681c9d21e54efe82a8b3b5aa39d9156
Benny24aa <harleyb101010@gmail.com>
2025-02-23 11:48:53 +0000

    joined hb list and file by unique HB code

--- Python Learning.py
+++ Python Learning.py
@@ -10,3 +10,10 @@ Diagnostics = Diagnostics[['MonthEnding', 'HBT','NumberOnList']]
 
 Diagnostics = Diagnostics.groupby(['HBT', 'MonthEnding'], as_index = False)['NumberOnList'].sum()
 ### Grouping By and Summing up Number on List Column Based on This
+
+#### Creating Dataframe for join
+
+HB_Look_Up = pd.DataFrame({'HBT':['S08000015'],
+'HBName':['A&G']})
+
+Diagnostics = pd.merge(Diagnostics, HB_Look_Up, how='left', on ='HBT') #### Using Pandas to join HBT to HB Name for test
