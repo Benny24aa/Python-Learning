a5cb3c8e3d503a061884834fab8af7bab27f38dd
Benny24aa <harleyb101010@gmail.com>
2025-02-23 09:28:21 +0000

    grouping by and summing number on list column

--- Python Learning.py
+++ Python Learning.py
@@ -1,6 +1,12 @@
 import pandas as pd
+
+
 Diagnostics = pd.read_csv('diagnostics_by_board_september_2024.csv') #### Reading Data
 Diagnostics = Diagnostics[Diagnostics['HBT'] == "S08000015"] ### Filtering
 
-Diagnostics = Diagnostics[['MonthEnding', 'DiagnosticTestType', 'DiagnosticTestDescription', 'WaitingTime', 'NumberOnList']]
+Diagnostics = Diagnostics[['MonthEnding', 'HBT','NumberOnList']]
 ### Selecting columns required, just like select in R
+
+
+Diagnostics = Diagnostics.groupby(['HBT', 'MonthEnding'], as_index = False)['NumberOnList'].sum()
+### Grouping By and Summing up Number on List Column Based on This
